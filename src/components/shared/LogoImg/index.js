d818a52403bbe50ef5213494edb1b0fb37cc6048
igorhenss <igorhenss@gmail.com>
2020-03-26 16:42:11 -0300

    Added spaces before and after word "logoImg".

--- src/components/shared/LogoImg/index.js
+++ src/components/shared/LogoImg/index.js
@@ -4,6 +4,6 @@ import logoImg from '../../../assets/logo.svg';
 
 export default function LogoImg() {
   return (
-    <img src={logoImg} alt="Be The Hero"/>
+    <img src={ logoImg } alt="Be The Hero"/>
   );
 }
